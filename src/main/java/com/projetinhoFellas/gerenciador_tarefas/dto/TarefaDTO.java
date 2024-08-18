3a401171c11ee448ccb55b125fb7351c2333f2f7
Lennon Calgaroto <lennoncalgaroto95@gmail.com>
2024-08-17 21:08:34 -0300

    Feature 02: Criação do crud.

---
+++ src/main/java/com/projetinhoFellas/gerenciador_tarefas/dto/TarefaDTO.java
@@ -0,0 +1,37 @@
+package com.projetinhoFellas.gerenciador_tarefas.dto;
+
+import com.projetinhoFellas.gerenciador_tarefas.entity.Tarefa;
+import com.projetinhoFellas.gerenciador_tarefas.enumTarefa.enumTarefaPrioridade;
+import com.projetinhoFellas.gerenciador_tarefas.enumTarefa.enumTarefaStatus;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class TarefaDTO {
+    private long id;
+    private String titulo;
+    private String descricao;
+    private enumTarefaStatus status;
+    private enumTarefaPrioridade prioridade;
+    private LocalDateTime dataCriacao;
+    private LocalDateTime dataAlteracao;
+    private LocalDateTime dataConclusao;
+
+    public static TarefaDTO of(Tarefa tarefa) {
+        return TarefaDTO.builder()
+                .id(tarefa.getId())
+                .titulo(tarefa.getTitulo())
+                .descricao(tarefa.getDescricao())
+                .status(tarefa.getStatus())
+                .prioridade(tarefa.getPrioridade())
+                .dataCriacao(tarefa.getDataCriacao())
+                .dataAlteracao(tarefa.getDataAlteracao())
+                .dataConclusao(tarefa.getDataConclusao())
+                .build();
+    }
+}
---
+++ src/main/java/com/projetinhoFellas/gerenciador_tarefas/service/TarefaService.java
@@ -0,0 +1,82 @@
+package com.projetinhoFellas.gerenciador_tarefas.service;
+
+import com.projetinhoFellas.gerenciador_tarefas.dto.TarefaDTO;
+import com.projetinhoFellas.gerenciador_tarefas.entity.Tarefa;
+import com.projetinhoFellas.gerenciador_tarefas.repository.TarefaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TarefaService {
+
+    @Autowired
+    private TarefaRepository tarefaRepository;
+
+    public void criarTarefa(TarefaDTO tarefaDTO) {
+        Tarefa tarefa = converterDTOParaEntidade(tarefaDTO);
+        tarefa = tarefaRepository.save(tarefa);
+        TarefaDTO.of(tarefa);
+    }
+
+    public void atualizarTarefa(Long id, TarefaDTO tarefaDTO) {
+        Tarefa tarefaExistente = tarefaRepository.findbyId(id)
+                .orElseThrow(()-> new RuntimeException("Tarefa não encontrada"));
+
+        tarefaExistente.setTitulo(tarefaDTO.getTitulo());
+        tarefaExistente.setDescricao(tarefaDTO.getDescricao());
+        tarefaExistente.setStatus(tarefaDTO.getStatus());
+        tarefaExistente.setPrioridade(tarefaDTO.getPrioridade());
+        tarefaExistente.setDataAlteracao(tarefaDTO.getDataAlteracao());
+
+        Tarefa tarefaAtualizada = tarefaRepository.save(tarefaExistente);
+        TarefaDTO.of(tarefaAtualizada);
+    }
+
+    public TarefaDTO buscarTarefasPorId(Long id) {
+        Tarefa tarefa = tarefaRepository.findbyId(id)
+                .orElseThrow(()-> new RuntimeException("Tarefa não encontrada"));
+        TarefaDTO.of(tarefa);
+        return converterEntidadeParaDTO(tarefa);
+    }
+
+    public List<TarefaDTO> listarTodasTarefas() {
+        List<Tarefa> tarefas = tarefaRepository.findAll();
+        return tarefas.stream()
+                .map(this::converterEntidadeParaDTO)
+                .collect(Collectors.toList());
+    }
+
+    public void deleteById(Long id) {
+        tarefaRepository.deleteById(id);
+    }
+
+    private TarefaDTO converterEntidadeParaDTO(Tarefa tarefa) {
+        TarefaDTO tarefaDTO = new TarefaDTO();
+        tarefaDTO.setId(tarefa.getId());
+        tarefaDTO.setTitulo(tarefa.getTitulo());
+        tarefaDTO.setDescricao(tarefa.getDescricao());
+        tarefaDTO.setStatus(tarefa.getStatus());
+        tarefaDTO.setPrioridade(tarefa.getPrioridade());
+        tarefaDTO.setDataCriacao(tarefa.getDataCriacao());
+        tarefaDTO.setDataAlteracao(tarefa.getDataAlteracao());
+        tarefaDTO.setDataConclusao(tarefa.getDataConclusao());
+        return tarefaDTO;
+    }
+
+    private Tarefa converterDTOParaEntidade(TarefaDTO tarefaDTO) {
+        Tarefa tarefa = new Tarefa();
+        tarefa.setId(tarefaDTO.getId());
+        tarefa.setTitulo(tarefaDTO.getTitulo());
+        tarefa.setDescricao(tarefaDTO.getDescricao());
+        tarefa.setStatus(tarefaDTO.getStatus());
+        tarefa.setPrioridade(tarefaDTO.getPrioridade());
+        tarefa.setDataCriacao(tarefaDTO.getDataCriacao());
+        tarefa.setDataAlteracao(tarefaDTO.getDataAlteracao());
+        tarefa.setDataConclusao(tarefaDTO.getDataConclusao());
+        return tarefa;
+    }
+
+}
