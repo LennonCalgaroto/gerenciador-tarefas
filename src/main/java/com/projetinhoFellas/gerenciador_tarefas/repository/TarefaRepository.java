d117e1adbf42c162824794eeaa449ddae02016cb
Lennon Augusto Calgaroto <lennon.calgaroto@emiteai.com.br>
2024-08-20 13:16:53 -0300

    Feature 04: Ajustes no repository e services

--- src/main/java/com/projetinhoFellas/gerenciador_tarefas/repository/TarefaRepository.java
+++ src/main/java/com/projetinhoFellas/gerenciador_tarefas/repository/TarefaRepository.java
@@ -1,31 +1,10 @@
 package com.projetinhoFellas.gerenciador_tarefas.repository;
 
-import com.projetinhoFellas.gerenciador_tarefas.entity.Tarefa;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
-public class TarefaRepository {
+public interface TarefaRepository {
 
-    @Autowired
-    private ITarefaRepository IrepositoryTarefa;
+    //Aqui posso colocar metodos mais complexos que não tem no JPARepository
 
-    public Tarefa save(Tarefa tarefa) {
-        return IrepositoryTarefa.save(tarefa);
-    }
-
-    public Optional<Tarefa> findById(Long id) {
-        return IrepositoryTarefa.findById(id);
-    }
-
-    public List<Tarefa> findAll() {
-        return IrepositoryTarefa.findAll();
-    }
-
-    public void deleteById(Long id) {
-        IrepositoryTarefa.deleteById(id);
-    }
 }
