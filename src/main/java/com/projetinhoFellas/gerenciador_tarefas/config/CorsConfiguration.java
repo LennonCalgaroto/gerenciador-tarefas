2f662639fcac6aa2e8092189c645fc5b42e9b4d1
Lennon Augusto Calgaroto <lennon.calgaroto@emiteai.com.br>
2024-08-22 21:44:10 -0300

    Feature 05: criação do CORS

---
+++ src/main/java/com/projetinhoFellas/gerenciador_tarefas/config/CorsConfiguration.java
@@ -0,0 +1,15 @@
+package com.projetinhoFellas.gerenciador_tarefas.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*") // Adicione o URL do seu front-end aqui
+                .allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*");
+    }
+}
